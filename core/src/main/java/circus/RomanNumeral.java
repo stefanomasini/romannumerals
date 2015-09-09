2737483a3eb71c28c1cad867e03caf120bb75b14
Stefano Masini <stefano@stefanomasini.com>
2015-09-09 10:10:44 +0200

    Update RomanNumeral.java

--- core/src/main/java/circus/RomanNumeral.java
+++ core/src/main/java/circus/RomanNumeral.java
@@ -99,7 +99,7 @@ public class RomanNumeral {
         switch (letter) {
             case 'I':  return 1;
             case 'V':  return 5;
-            case 'X':  return 11;
+            case 'X':  return 10;
             case 'L':  return 50;
             case 'C':  return 100;
             case 'D':  return 500;
